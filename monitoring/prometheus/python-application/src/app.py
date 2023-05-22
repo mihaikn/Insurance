2478321ff2899551bb06535e096e9a7a5ce13647
mihaikn <mihamar113@gmail.com>
2023-05-22 04:34:08 +0300

    1

--- monitoring/prometheus/python-application/src/app.py
+++ monitoring/prometheus/python-application/src/app.py
@@ -1,12 +1,19 @@
-from flask import Flask,request,make_response,jsonify
+from flask import Flask,request,make_response,jsonify,Response
 from flask_sqlalchemy import SQLAlchemy
 from config_ import *
+import prometheus_client
+from prometheus_client.core import CollectorRegistry
+from prometheus_client import Summary, Counter, Histogram, Gauge
+import time
 
 app=Flask(__name__)
 app.config.from_object(Production)
+_INF = float("inf")
+graphs={}
+graphs['c']=Counter('python_request_operations_total','The total number of processed requests')
+graphs['h']= Histogram('python_request_duration_seconds','Histogram for the duration in seconds',buckets=(1,2,5,6,10,_INF))
 
 db=SQLAlchemy(app)
-
 class Programm(db.Model):
     __tablename__='programms'
     id=db.Column(db.Integer,primary_key=True)
@@ -18,7 +25,6 @@ class Programm(db.Model):
         self.prog_name=prog_name
         self.direction=direction
         self.cost=cost
-
 
 @app.route('/addProgram',methods=['POST'])
 def addProgram():
@@ -32,6 +38,8 @@ def addProgram():
     return make_response(jsonify(res),200)
 @app.route('/allprogs',methods=['GET'])
 def allprogs():
+    start=time.time()
+    graphs['c'].inc()
     progs=db.session.query(Programm)
     res=[]
     for a in progs:
@@ -40,6 +48,8 @@ def allprogs():
         tmp.append(a.direction)
         tmp.append(a.cost)
         res.append(tmp)
+    end=time.time()
+    graphs['h'].observe(end-start)
     return make_response(jsonify(res),200)
 @app.route('/electiveprogs/<dir>',methods=['GET'])
 def electiveprogs(dir):
@@ -56,8 +66,15 @@ def electiveprogs(dir):
 def creditcalc(prog):
     progs=db.session.query(Programm).filter(Programm.prog_name==prog).first()
     return make_response(jsonify(progs.cost/12),200)
-if __name__=='__main__':
-    app.run()
+@app.route('/metrics')
+def metrics():
+    res=[]
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+
+    return Response(res,mimetype="text/plain")
+# if __name__=='__main__':
+#     app.run()
 
 
 
