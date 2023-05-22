eec618e64bcfbba3e12033de1a1d70cc2fb354e9
mihaikn <55029613+mihaikn@users.noreply.github.com>
2023-05-22 23:47:49 +0300

    Add files via upload

--- monitoring/prometheus/python-application/src/config_.py
+++ monitoring/prometheus/python-application/src/config_.py
@@ -1,14 +1,14 @@
 class Config(object):
     DEBUG = False
     TESTING=False
-    SQLALCHEMY_DATABASE_URI='postgresql://postgres:admin@localhost/programs'
+    SQLALCHEMY_DATABASE_URI='postgresql://admin:root@127.0.0.1:5431/programms'
 class Test(Config):
     TESTING=False
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='postgresql://postgres:admin@localhost/programs'
 class Production(Config):
     TESTING=False
-    SQLALCHEMY_DATABASE_URI='postgresql://admin:root@127.0.0.1:5431/programms'
+    SQLALCHEMY_DATABASE_URI='postgresql://admin:root@postgre:5432/programms'
     DEBUG=False
 class Razrab(Config):
     TESTING=False
